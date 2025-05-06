ddb2ceb1cc8f571d3c77b00f985ea16173f2feab
Jason Golden <jasonwilliamgolden@gmail.com>
2025-05-05 20:38:25 -0400

    Fix useMedSpaData import for successful build

--- medspa-dashboard/src/components/ComparisonGrid.jsx
+++ medspa-dashboard/src/components/ComparisonGrid.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { useMedSpaData } from '../hooks/useMedSpaData';
+import useMedSpaData from '../hooks/useMedSpaData';
 
 const columns = [
   { field: 'treatment', headerName: 'Treatment', width: 200 },
